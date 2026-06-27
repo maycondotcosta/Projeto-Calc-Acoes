7fb5651fe3c2c0ba8c4c120b7dea988ba8b8ec0b
Maycon <mayconcosta@gmail.com>
2026-06-27 12:12:59 -0300

    corrigindo erro de porta

--- app/__init__.py
+++ app/__init__.py
@@ -5,17 +5,14 @@ from .models import db
 def create_app():
     app = Flask(__name__)
     
-    # URL do banco vinda do Render (Environment Variables)
-    database_url = os.environ.get('DATABASE_URL')
-    
-    # Corrige se a URL começar com 'postgres://' para 'postgresql://'
-    if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    # URL do banco
+    db_url = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    db.init_app(app)
+    # INICIALIZAÇÃO "PREGUIÇOSA"
+    # Comente a linha abaixo para ver se o erro desaparece
+    # db.init_app(app)
     
     from .routes import main_bp
     app.register_blueprint(main_bp)
